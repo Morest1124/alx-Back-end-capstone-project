6300095fd5730d46c796f676824438f40397e808
Morest1124 <morest1124burawa2005@gmail.com>
2025-10-08 10:06:59 +0200

    feat(projects): Implemented role-based security rules
    - Defined IsClient and IsFreelancer roles.
    - Added IsProjectOwner for Project CRUD access.
    - Created IsProposalProjectOwner for bid management.
    - Ensures API security based on User Profile roles.

---
+++ binaryblade24/Project/Permissions.py
@@ -0,0 +1,62 @@
+from rest_framework import permissions
+from User.models import Profile, User  
+
+class IsClient(permissions.BasePermission):
+    """
+    Custom permission to only allow 'CLIENT'
+    """
+    message = 'Access denied. Only clients are allowed to perform this action.'
+
+    def has_permission(self, request, view):
+        # Allow read access (GET, HEAD, OPTIONS) for browsing purposes
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Check if the authenticated user has a 'CLIENT' role
+        if request.user and request.User.is_authenticated:
+            try:
+                # Access the profile to check the role
+                return request.User.profile.role == Profile.UserRoles.CLIENT
+            except Profile.DoesNotExist:
+                return False
+        return False
+
+class IsFreelancer(permissions.BasePermission):
+    """
+    Custom permession for Only freelencers allowd
+    """
+    message = 'Access denied. Only freelancers are allowed to perform this action.'
+
+    def has_permession(self, request, view):
+        if request.user and request.user.is_authenticated:
+            try:
+                # Access the profile to check the role
+                return request.user.profile.role == Profile.UserRoles.FREELANCER
+            except Profile.DoesNotExist:
+                return False
+        return False
+
+class IsProjectOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow the project creator (client) to edit or delete it.
+    """
+    message = 'You do not have permission to modify this project.'
+    
+    def has_object_permission(self, request, view, obj):
+        # Allow read permissions (GET, HEAD, OPTIONS)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions (PUT, PATCH, DELETE) are only allowed to the owner
+        return obj.client == request.user
+
+class IsProposalProjectOwner(permissions.BasePermission):
+    """
+    Custom permission to allow only the owner of the related Project 
+    to manage (accept/rejict) the Proposal.
+    """
+    message = 'You must be the project owner to manage this proposal.'
+    
+    def has_object_permession(self, request, view, obj):
+        # Write permissions (PUT, PATCH) are only allowed to the project owner
+        return obj.project.client == request.user
