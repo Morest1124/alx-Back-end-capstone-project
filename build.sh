6c5ed63e1435835a788f46688a465b00b2b60bc1
Morest1124 <morest1124burawa2005@gmail.com>
2025-10-21 15:46:54 +0200

    feat: Configure project for Render deployment

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python binaryblade24/manage.py collectstatic --no-input
+python binaryblade24/manage.py migrate
