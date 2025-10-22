7c00b03b7e6abb195e3db5ba46a0d2b299d06755
Morest1124 <morest1124burawa2005@gmail.com>
2025-10-22 07:06:45 +0200

    feat(temp): add createsuperuser to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python binaryblade24/manage.py collectstatic --no-input
 python binaryblade24/manage.py migrate
+
+# Create superuser
+python binaryblade24/manage.py createsuperuser --no-input --username admin --email admin@example.com
