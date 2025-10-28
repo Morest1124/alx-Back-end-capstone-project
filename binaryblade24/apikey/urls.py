ca128b52db0c78a5e3e714112cb83c62539417bc
Morest1124 <morest1124burawa2005@gmail.com>
2025-10-28 16:31:13 +0200

    inclueded app_name = 'apikey'

--- binaryblade24/apikey/urls.py
+++ binaryblade24/apikey/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import GenerateAPIKeyView
 
+app_name = 'apikey'
+
 urlpatterns = [
     path('generate-key/', GenerateAPIKeyView.as_view(), name='generate-api-key'),
 ]
